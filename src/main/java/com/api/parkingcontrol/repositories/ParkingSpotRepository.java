4622c39b868eb50897b0090ed8b8f67d7d777362
Ismael Gomes <ismael.gomes@clinicasim.com>
2022-07-12 21:31:33 -0300

    finishing CRUD

--- src/main/java/com/api/parkingcontrol/repositories/ParkingSpotRepository.java
+++ src/main/java/com/api/parkingcontrol/repositories/ParkingSpotRepository.java
@@ -1,6 +1,20 @@
 package com.api.parkingcontrol.repositories;
 
+import com.api.parkingcontrol.models.ParkingSpotModel;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface ParkingSpotRepository extends JpaRepository {
+import java.awt.print.Pageable;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface ParkingSpotRepository extends JpaRepository <ParkingSpotModel, UUID> {
+    boolean existsByLicensePlateCar(String licensePlateCar);
+    boolean existsByParkingSpotNumber(String parkingSpotNumber);
+    boolean existsByApartmentAndBlock(String apartment, String block);
+    Page<ParkingSpotModel> findAll(Pageable pageable);
+    Optional<ParkingSpotModel> findById(UUID id);
+    void delete(ParkingSpotModel parkingSpotModel);
 }
